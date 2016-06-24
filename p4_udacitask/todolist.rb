6a3a304bdc66ee0f6b0b3138fa79f75653ea894f
Greg Berger <grberger@usc.edu>
2016-06-24 14:04:29 -0700

    Finished project and context features

--- p4_udacitask/todolist.rb
+++ p4_udacitask/todolist.rb
@@ -1,5 +1,8 @@
+require 'date'
+
 class TodoList
   attr_reader :title, :items
+  attr_writer :title
 
   def initialize(title)
     @title = title
@@ -7,16 +10,55 @@ class TodoList
   end
 
   def add_item(description, due_date, project, context)
-    item = Item.new(description, due_date, project, context)
+    item = Item.new(items.size, description, due_date, project, context)
     @items.push(item)
+  end
+
+  def delete_item_at(index)
+    @items.delete_at(index)
+  end
+
+  def mark_complete_on_item(id)
+    @items.each do |item|
+      if item.id == id
+        item.isComplete = true
+      end
+    end
+  end
+
+  def print_list
+    puts "******** #{@title} Tasks ********"
+    @items.each do |item|
+      puts "Task ID: #{item.id}\nDescription: #{item.description}\nProject: #{item.project}\nContext: #{item.context}\nDue: #{item.due_date}\nComplete? #{item.isComplete}\n\n"
+    end
+  end
+
+  def print_list_by_project(project)
+    puts "******** #{@title} Tasks by Project: #{project} ********"
+    @items.each do |item|
+      if item.project == project
+        puts "Task ID: #{item.id}\nDescription: #{item.description}\nProject: #{item.project}\nContext: #{item.context}\nDue: #{item.due_date}\nComplete? #{item.isComplete}\n\n"
+      end
+    end
+  end
+  
+  def print_list_by_context(context)
+    puts "******** #{@title} Tasks by Context: #{context} ********"
+    @items.each do |item|
+      if item.context == context
+        puts "Task ID: #{item.id}\nDescription: #{item.description}\nProject: #{item.project}\nContext: #{item.context}\nDue: #{item.due_date}\nComplete? #{item.isComplete}\n\n"
+      end
+    end
   end
 
 end
 
 class Item
-  attr_reader :description, :isComplete, :due_date, :project, :context
+  attr_reader :id, :description, :isComplete, :due_date, :project, :context
+  attr_writer :isComplete
   
-  def initialize(description, due_date, project, context)
+  def initialize(id, description, due_date, project, context)
+    @id = id
     @description = description
     @due_date = due_date
     @project = project
--- p4_udacitask/udacitask.rb
+++ p4_udacitask/udacitask.rb
@@ -1,23 +1,48 @@
 require_relative 'todolist.rb'
+require 'date'
 
 # Creates a new todo list
+todo_list = TodoList.new("Udacitask Manager")
+
+# Print header message
+puts "Welcome to #{todo_list.title}\n\n"
 
 # Add four new items
+todo_list.add_item("Buy a smartphone", Date.new(2016,6,27), "Find a Wife", "Errands")
+todo_list.add_item("Download Tinder", Date.new(2016,6,28), "Find a Wife", "Phone")
+todo_list.add_item("Buy workout gear", Date.new(2016,6,29), "Get Fit", "Errands")
+todo_list.add_item("Think of excuse to avoid workout", Date.new(2016,6,26), "Get Fit", "On Couch")
 
 # Print the list
+todo_list.print_list
+
+# New Feature: List by project
+todo_list.print_list_by_project("Get Fit")
+
+# New Feature: List by context
+todo_list.print_list_by_context("Errands")
 
 # Delete the first item
+todo_list.delete_item_at(0)
 
 # Print the list
+todo_list.print_list
 
 # Delete the second item
+todo_list.delete_item_at(0)
 
 # Print the list
+todo_list.print_list
 
 # Update the completion status of the first item to complete
+todo_list.mark_complete_on_item(2)
 
 # Print the list
+todo_list.print_list
 
 # Update the title of the list
+todo_list.title = "Udacitask 2.0"
 
 # Print the list
+todo_list.print_list
+
